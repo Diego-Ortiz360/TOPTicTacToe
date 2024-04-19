c83780c98955fdbc3bfc8d772b64828b8648c662
Diego Ortiz <73370653+Diego-Ortiz360@users.noreply.github.com>
2024-04-19 11:43:24 -0300

    ADD: Conditions to win round and game now working
    Added cleanGameBoard method on gameBoard
    Added saveRoundResult method on gameController

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const gameBoard = (function (){
 
     let gameBoard = new Array(9);
 
+    const cleanGameBoard = () => gameBoard = new Array (9); 
+
     const checkPosition = (position) =>
     {
             return gameBoard[position];   
@@ -19,14 +21,14 @@ const gameBoard = (function (){
 
     const checkIfWin = () => 
     ( 
-        checkLine(0,1,2) ||
-        checkLine(3,4,5) ||
-        checkLine(6,7,8) ||
-        checkLine(0,4,8) ||
-        checkLine(6,4,2) ||
-        checkLine(0,3,6) ||
-        checkLine(1,4,7) ||
-        checkLine(2,4,8) 
+        checkLine(0,1,2) || // Row 1
+        checkLine(3,4,5) || // Row 2
+        checkLine(6,7,8) || // Row 3
+        checkLine(0,4,8) || // Diagonal 1
+        checkLine(2,4,6) || // Diagonal 2
+        checkLine(0,3,6) || // Column 1
+        checkLine(1,4,7) || // Column 2
+        checkLine(2,5,8)  // Column 3
     )
     
     const checkIfHasEmptyCell = () => gameBoard.includes(undefined);
@@ -37,7 +39,7 @@ const gameBoard = (function (){
         gameBoard[position] = playersymbol;
     }
     
-    return {checkPosition, checkLine, checkIfWin, checkIfHasEmptyCell, markMove};
+    return {checkPosition, checkLine, checkIfWin, checkIfHasEmptyCell, markMove, cleanGameBoard};
 })();
 
 
@@ -62,11 +64,34 @@ const gameController = (function (gameBoard){
 
     const winnerOfTheRound = (name) => console.log (`hey ${name} ganaste la ronda!`);
 
-    const tieRound = () => console.log ("Hey, es empate");
+    const tie = () => console.log ("Hey, es empate, por favor juega de nuevo");
 
-    const winnerOfTheGame = () => console.log ("Hey ganaste el juego");
+    const winnerOfTheGame = (name) => console.log (`Hey ${name} ganaste el juego!`);
 
     let roundResult = new Array();
+
+    const saveRoundResult = (value) => roundResult.push(value); 
+
+    const evaluateRoundResults = (arr) =>
+    {
+        const result = arr.reduce((acc, val) =>
+    {
+         if (!acc[val])
+        {
+            acc[val] = 1;
+        }
+        else 
+        {
+            acc[val]++;
+        }
+        return acc;
+    },{});
+
+    const winner = Object.keys(result).find(key => result[key] > 1 && key !==`tie`);
+
+    return ( winner || `tie` );
+    }
+
 
 
     const playerMove = (position, symbol, name) =>
@@ -75,22 +100,51 @@ const gameController = (function (gameBoard){
         {
             gameBoard.markMove(position, symbol);
 
+
             if (gameBoard.checkIfWin())
             {
                 winnerOfTheRound(name);
 
-                roundResult.push(name);
+                saveRoundResult(name);
+                console.log(`Se pushea ${name} en roundresult`);
 
-                console.log (roundResult);
+                gameBoard.cleanGameBoard();
+
             }
             else if (!gameBoard.checkIfHasEmptyCell())
             {
-                tieRound();
+                tie();
 
-                roundResult.push("tie");
+                saveRoundResult(`tie`);
+                console.log(`Se pushea "tie" en roundresult`);
+
+                gameBoard.cleanGameBoard();
+
             }
+
+            if (roundResult.length == 3)
+            {
+                if (evaluateRoundResults(roundResult) !== `tie` )
+                {
+                    
+                    winnerOfTheGame(name);
+
+                    console.log ("se ejecuta si roundresult.length == 3");
+
+                    gameBoard.cleanGameBoard();
+
+                }
+                else 
+                { 
+                    tie();
+                
+                    gameBoard.cleanGameBoard();
+                }
+            }
+
         }
         else {console.log("El casillero esta ocupado, por favor juega de nuevo");}
+
     }
 
     return {playerMove};
@@ -99,11 +153,29 @@ const gameController = (function (gameBoard){
 
 
 const player1 = createPlayer(gameController);
-
 player1.setName("Diego");
-
 player1.setSymbol("x");
 
+const player2= createPlayer(gameController);
+player2.setName("Skynet");
+player2.setSymbol("O")
+
 player1.move(0);
+
 player1.move(1);
+
+player1.move(2);
+
+
+player1.move(0);
+
+player1.move(1);
+
+player1.move(2);
+
+
+player1.move(0);
+
+player1.move(1);
+
 player1.move(2);
